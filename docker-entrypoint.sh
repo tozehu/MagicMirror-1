145543f42a4474e5eba9b407a1aaab1fdb04807b
CBrosius <Christian.Brosius@gmail.com>
2019-12-24 00:50:03 +0100

    Add Default-Config and EntryPoint
    Signed-off-by: CBrosius <Christian.Brosius@gmail.com>

---
+++ docker-entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -e
+
+if [ ! "$(ls -A /opt/magic_mirror/modules)" ]; then
+    cp -Rn /opt/default_modules/. /opt/magic_mirror/modules
+fi
+
+if [ ! "$(ls -A /opt/magic_mirror/config)" ]; then
+    cp /opt/magic_mirror/mm-docker-config.js /opt/magic_mirror/config/config.js
+fi
+
+exec "$@"
